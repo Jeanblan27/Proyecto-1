df522192a64665f6dbea978481e613a97f9b040c
Jeancarlo Blanco Mora <jeancarloblanco27@gmail.com>
2026-09-10 17:43:57 -0600

    cambio a main

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,41 @@
-package PACKAGE_NAME;
+import Presentation.Model.Funcionario.ListaFuncionarios;
+import Presentation.Model.Categoria.ListaCategorias;
+import Presentation.Model.Recurso.ListaRecursos;
+import Presentation.Model.Reserva.ListaReservas;
+import Presentation.View.Login;
+import javax.swing.SwingUtilities;
+
+import Presentation.Controller.ControladorLogin;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(() -> {
+
+            ListaFuncionarios modeloFuncionarios =
+                    new ListaFuncionarios();
+
+            ListaCategorias modeloCategorias =
+                    new ListaCategorias();
+
+            ListaRecursos modeloRecursos =
+                    new ListaRecursos( modeloCategorias.getCategorias());
+
+            ListaReservas modeloReservas =
+                    new ListaReservas();
+
+            Login vista = new Login();
+
+            new ControladorLogin(
+                    modeloFuncionarios,
+                    modeloCategorias,
+                    modeloRecursos,
+                    modeloReservas,
+                    vista
+            );
+
+            vista.mostrar();
+        });
+    }
 }
